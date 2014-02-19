cfda509670521d1bce2e8793372de086bed91c2e
Bogdan <chernichenkobogdan@gmail.com>
2014-02-19 18:36:48 +0200

    Update HelloController.java

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,16 +1,7 @@
-package com.springapp.mvc;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-@RequestMapping("/")
-public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello worl1234567890-d!");
-		return "in";
-	}
-}
+ public class HelloController {
+  	@RequestMapping(method = RequestMethod.GET)
+  	public String printWelcome(ModelMap model) {
+  		model.addAttribute("message", "Hello, world!!!!");
+  		return "hello";
+  	}
+  }
