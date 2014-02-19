099140d1313528acf1e3ee3d8a7acb81b213533d
Bogdan <chernichenkobogdan@gmail.com>
2014-02-19 18:52:35 +0200

    del

--- src/main/java/com/springapp/mvc/HelloController.java
+++
@@ -1,7 +0,0 @@
- public class HelloController {
-  	@RequestMapping(method = RequestMethod.GET)
-  	public String printWelcome(ModelMap model) {
-  		model.addAttribute("message", "Hello, world!!!!");
-  		return "hello";
-  	}
-  }
