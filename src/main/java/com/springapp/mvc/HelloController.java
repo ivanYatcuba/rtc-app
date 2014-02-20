f1e54c07b59076214d53d07897de162e61ab6c64
Bogdan <chernichenkobogdan@gmail.com>
2014-02-20 21:45:37 +0200

    page change

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -31,7 +31,7 @@ public class HelloController {
         @RequestMapping(value="/registration",method=RequestMethod.POST)
         public String registration(ModelMap model)
         {
-            return "registration.ftl";
+            return "registration";
         }
 
     @RequestMapping(value="/viewPage",method=RequestMethod.POST)
