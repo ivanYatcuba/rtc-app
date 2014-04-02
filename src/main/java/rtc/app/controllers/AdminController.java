091b47c7f533424b8ae793d9babb1b950913a7ec
LisitsaAlexandr <scorpter@mail.ru>
2014-04-02 16:09:06 +0300

    Update AdminController.java

--- src/main/java/rtc/app/controllers/AdminController.java
+++ src/main/java/rtc/app/controllers/AdminController.java
@@ -2,7 +2,7 @@ package rtc.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("adminController")
 @RequestMapping("/admin")
