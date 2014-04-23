686ba5be03fe62062bd29a665855edb65dc66ae8
alex9229 <mika9229@mail.ru>
2014-04-24 03:18:20 +0400

    delete bug

--- src/main/java/net/github/rtc/app/controller/common/LoginController.java
+++ src/main/java/net/github/rtc/app/controller/common/LoginController.java
@@ -44,9 +44,9 @@ public class LoginController {
     public void loginAttempt() {
         User currentUser = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (currentUser.hasRole(Roles.ROLE_ADMIN.name())) {
-         RedirectToRoute("/admin");
+         
         } else {
-          RedirectToRoute("/");
+       
         }
     }
     
