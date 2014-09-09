76a8daf5c8e326431362f5396eac29cc81704bed
bsoBerra <stanislav.berdnik@gmail.com>
2014-09-09 17:21:18 +0300

    added logs in JobRemovingUser

--- src/main/java/net/github/rtc/app/export/JobRemovingUser.java
+++ src/main/java/net/github/rtc/app/export/JobRemovingUser.java
@@ -1,6 +1,8 @@
 package net.github.rtc.app.export;
 
 import net.github.rtc.app.model.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import net.github.rtc.app.service.UserService;
@@ -10,13 +12,16 @@ import java.util.Collection;
 
 @Component
 public class JobRemovingUser {
+    private static Logger LOG = LoggerFactory.getLogger(JobRemovingUser.class.getName());
 
     @Autowired
     private UserService userService;
 
     @Scheduled(fixedDelay =300000)
     public void deletingUser(){
+        LOG.info("JobRemovingUser started");
         Collection<User> listUsers = userService.findAll();
+        LOG.info("Size of downloaded collection: {}", listUsers.size());
         for(User user: listUsers){
             if(user.getRemovalDate() != null){
                String userCode = user.getCode();
