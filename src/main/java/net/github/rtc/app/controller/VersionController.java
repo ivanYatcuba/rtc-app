42069199d5a82d13dd0d18b7b7a9657959dc52c4
Kostya Gavrilchenko <12fthofmay@gmail.com>
2014-09-10 15:57:53 +0300

    Added controller and view to display last git commit

---
+++ src/main/java/net/github/rtc/app/controller/VersionController.java
@@ -0,0 +1,14 @@
+package net.github.rtc.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("versionController")
+public class VersionController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    String index() {
+        return "version";
+    }
+}
