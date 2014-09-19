0d7f585f3658204dce4b3ad785ed3d909dee6ccc
Ivan Yatcuba <ivan.yatsouba@gmail.com>
2014-09-20 02:31:04 +0300

    checkstyle

--- src/main/java/net/github/rtc/app/model/user/RoleType.java
+++ src/main/java/net/github/rtc/app/model/user/RoleType.java
@@ -5,10 +5,14 @@ package net.github.rtc.app.model.user;
  * @author Vladislav Pikus
  */
 public enum RoleType {
-    ROLE_USER("User") ,ROLE_ADMIN("Admin"), ROLE_EXPERT("Expert");
-
+    ROLE_USER("User"), ROLE_ADMIN("Admin"), ROLE_EXPERT("Expert");
     private final String roleViewName;
 
-    RoleType(final String s) {roleViewName = s;}
-    public String getRoleViewName() {return roleViewName;}
+    RoleType(final String s) {
+        roleViewName = s;
+    }
+
+    public String getRoleViewName() {
+        return roleViewName;
+    }
 }
