6d2b16d69eaffa3a23898d91e2d6856df9dfefe2
denis <denis.v.kovalenko@gmail.com>
2014-10-14 20:07:25 +0300

    Annotations in service beans changed to @Service

--- src/main/java/net/github/rtc/app/service/impl/CodeGenServiceImpl.java
+++ src/main/java/net/github/rtc/app/service/impl/CodeGenServiceImpl.java
@@ -2,13 +2,14 @@ package net.github.rtc.app.service.impl;
 
 import net.github.rtc.app.service.CodeGenService;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 /**
  * Created by denis on 13.10.14.
  */
-@Repository
+@Service("CodeGenerator")
 public class CodeGenServiceImpl implements CodeGenService {
     public String generateCode() {
         return UUID.randomUUID().toString();
--- src/main/java/net/github/rtc/app/service/impl/EncoderServiceImpl.java
+++ src/main/java/net/github/rtc/app/service/impl/EncoderServiceImpl.java
@@ -3,11 +3,12 @@ package net.github.rtc.app.service.impl;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by denis on 13.10.14.
  */
-@Repository
+@Service("EncoderService")
 public class EncoderServiceImpl implements net.github.rtc.app.service.EncoderService {
     public String encode(CharSequence rawPassword) {
         final PasswordEncoder encoder = new StandardPasswordEncoder();
