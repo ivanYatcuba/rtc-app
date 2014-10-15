92c173472ede8d40ee177368a51c9cfce950ca23
Stanislav <stanislav.berdnik@gmail.com>
2014-10-15 15:36:58 +0300

    expanded methods in search controller

--- src/main/java/net/github/rtc/app/controller/admin/SearchController.java
+++ src/main/java/net/github/rtc/app/controller/admin/SearchController.java
@@ -1,14 +1,23 @@
 package net.github.rtc.app.controller.admin;
 
-import net.github.rtc.app.utils.datatable.search.CourseSearchFilter;
-import net.github.rtc.app.utils.datatable.search.NewsSearchFilter;
-import net.github.rtc.app.utils.datatable.search.ReportSearchFilter;
-import net.github.rtc.app.utils.datatable.search.UserSearchFilter;
+import net.github.rtc.app.model.course.Course;
+import net.github.rtc.app.model.course.CourseType;
+import net.github.rtc.app.model.news.News;
+import net.github.rtc.app.model.news.NewsStatus;
+import net.github.rtc.app.model.user.RoleType;
+import net.github.rtc.app.model.user.User;
+import net.github.rtc.app.service.CourseService;
+import net.github.rtc.app.service.NewsService;
+import net.github.rtc.app.service.UserService;
+import net.github.rtc.app.utils.datatable.search.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
 
 /**
  * Created by Berdniky on 15.10.2014.
@@ -20,6 +29,21 @@ public class SearchController {
 
     private static final String ROOT = "portal/admin";
     private static final String STRING_SEARCH_PAGE = "/search/tables";
+    private static final String STRING_NEWS = "news";
+    private static final String STRING_STATUSES = "statuses";
+    private static final String STRING_FILTER_NEWS = "filterNews";
+    private static final String STRING_TYPES = "types";
+    private static final String STRING_FILTER_COURSE = "filterCourse";
+    private static final String STRING_USERS = "users";
+    private static final String STRING_AUTHORITIES = "authorities";
+    private static final String STRING_FILTER_USER = "filterUser";
+
+    @Autowired
+    private NewsService newsService;
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public String searchPage() {
@@ -27,20 +51,36 @@ public class SearchController {
     }
 
     @RequestMapping(value = "/newsTable", method = RequestMethod.POST)
-    public ModelAndView getNewsTable() {
+    public ModelAndView getNewsTable (@ModelAttribute("filterNews") final NewsSearchFilter filterNews) {
         final ModelAndView mav = new ModelAndView(ROOT + STRING_SEARCH_PAGE + "/newsSearchTable");
+        final SearchResults<News> results = newsService.search(filterNews);
+        mav.addAllObjects(results.getPageModel());
+        mav.addObject(STRING_NEWS, results.getResults());
+        mav.addObject(STRING_STATUSES, getStatuses());
+        mav.addObject(STRING_FILTER_NEWS, filterNews);
         return mav;
     }
 
     @RequestMapping(value = "/courseTable", method = RequestMethod.POST)
-    public ModelAndView getCourseTable() {
+    public ModelAndView getCourseTable(@ModelAttribute("filterCourse") final CourseSearchFilter filterCourse) {
         final ModelAndView mav = new ModelAndView();
+        final SearchResults<Course> results = courseService.search(filterCourse);
+        mav.addAllObjects(results.getPageModel());
+        mav.addObject("courses", results.getResults());
+        mav.addObject(STRING_TYPES, CourseType.findAll());
+        mav.addObject(STRING_STATUSES, getStatuses());
+        mav.addObject(STRING_FILTER_COURSE, filterCourse);
         return mav;
     }
 
     @RequestMapping(value = "/userTable", method = RequestMethod.POST)
-    public ModelAndView getUserTable() {
+    public ModelAndView getUserTable(@ModelAttribute("filterUser") final UserSearchFilter userFilter) {
         final ModelAndView mav = new ModelAndView();
+        final SearchResults<User> results = userService.search(userFilter);
+        mav.addAllObjects(results.getPageModel());
+        mav.addObject(STRING_USERS, results.getResults());
+        mav.addObject(STRING_AUTHORITIES, getAuthorities());
+        mav.addObject(STRING_FILTER_USER, userFilter);
         return mav;
     }
 
@@ -68,6 +108,16 @@ public class SearchController {
     @ModelAttribute("reportFilter")
     public ReportSearchFilter getReportSearchFilter() {
         return new ReportSearchFilter();
+    }
+
+    @ModelAttribute(STRING_STATUSES)
+    public Collection<String> getStatuses() {
+        return NewsStatus.findAll();
+    }
+
+    @ModelAttribute(STRING_AUTHORITIES)
+    public Collection<String> getAuthorities() {
+        return RoleType.findAll();
     }
 
 }
