e1d3416c204159f5e67dfcfbb826bf23ae90ea88
Stanislav <stanislav.berdnik@gmail.com>
2014-10-30 13:12:32 +0200

    fixed project tree

--- src/main/java/net/github/rtc/app/controller/admin/UserController.java
+++ src/main/java/net/github/rtc/app/controller/admin/UserController.java
@@ -3,7 +3,7 @@ package net.github.rtc.app.controller.admin;
 import net.github.rtc.app.model.user.RoleType;
 import net.github.rtc.app.model.user.User;
 import net.github.rtc.app.service.UserService;
-import net.github.rtc.app.utils.upload.FileUpload;
+import net.github.rtc.app.utils.files.upload.FileUpload;
 import net.github.rtc.app.utils.datatable.search.SearchResults;
 import net.github.rtc.app.utils.datatable.search.UserSearchFilter;
 import net.github.rtc.app.utils.propertyeditors.CustomRoleEditor;
@@ -140,11 +140,11 @@ public class UserController {
     @RequestMapping(value = "/getAdmins", method = RequestMethod.POST)
     public
     @ResponseBody
-    List<String> getAllAdmins() {
-        final List<String> results = new ArrayList<>();
+    Map<String, Long> getAdminMapDataId() {
+        final Map<String, Long> results = new HashMap<>();
         final List<User> admins = userService.getUserByRole(RoleType.ROLE_ADMIN);
         for (final User admin : admins) {
-            results.add(admin.shortString());
+            results.put(admin.shortString(), admin.getId());
         }
         return results;
     }
