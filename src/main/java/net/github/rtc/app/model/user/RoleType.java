1eb80fb9b97e35ec22956b966262b7ad41295550
denis <denis.v.kovalenko@gmail.com>
2014-10-31 13:03:17 +0200

    Removed "ALL" field from enums

--- src/main/java/net/github/rtc/app/model/user/RoleType.java
+++ src/main/java/net/github/rtc/app/model/user/RoleType.java
@@ -8,21 +8,22 @@ import java.util.List;
  * @author Vladislav Pikus
  */
 public enum RoleType {
-    ALL("ALL"), ROLE_USER("User"), ROLE_ADMIN("Administrator"), ROLE_EXPERT("Expert");
+    ROLE_USER("User"), ROLE_ADMIN("Administrator"), ROLE_EXPERT("Expert");
     private final String roleViewName;
 
     RoleType(final String s) {
         roleViewName = s;
     }
 
-    public String getRoleViewName() {
-        return roleViewName;
-    }
     public static List<String> findAll() {
         final List<String> res = new ArrayList<>();
         for (final RoleType status : RoleType.values()) {
             res.add(status.name());
         }
         return res;
+    }
+
+    public String getRoleViewName() {
+        return roleViewName;
     }
 }
