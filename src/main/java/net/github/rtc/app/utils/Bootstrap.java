72ea52aa3e5ac18b1fe7183b83ea6b935045c053
Stanislav <stanislav.berdnik@gmail.com>
2014-11-26 13:43:55 +0200

    checkstyle

--- src/main/java/net/github/rtc/app/utils/Bootstrap.java
+++ src/main/java/net/github/rtc/app/utils/Bootstrap.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 
 @Component
