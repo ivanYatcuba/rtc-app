4723c7819ea63b84b59a89d9ed2fa2895d334b0f
r0ndom <r0ndom@yahoo.com>
2015-01-08 17:16:14 +0200

    checkstyle

--- src/main/java/net/github/rtc/app/utils/freemarker/AdditionalTagsRenderer.java
+++ src/main/java/net/github/rtc/app/utils/freemarker/AdditionalTagsRenderer.java
@@ -14,19 +14,19 @@ public class AdditionalTagsRenderer {
 
     public String renderAdditionalTags(BindStatus br, Map<String, Object> model) {
         String attrs = "";
-        String beanName = br.getPath().substring(0, br.getPath().indexOf("."));
-        String fieldName = br.getExpression();
-        Object bean = model.get(beanName);
+        final String beanName = br.getPath().substring(0, br.getPath().indexOf("."));
+        final String fieldName = br.getExpression();
+        final Object bean = model.get(beanName);
         if (bean != null) {
-            Class clazz = model.get(beanName).getClass();
+            final Class clazz = model.get(beanName).getClass();
             try {
-                Field field = clazz.getDeclaredField(fieldName);
+                final Field field = clazz.getDeclaredField(fieldName);
                 if (field.isAnnotationPresent(Length.class)) {
-                    Length length = field.getAnnotation(Length.class);
+                    final Length length = field.getAnnotation(Length.class);
                     attrs = "maxlength=\"" + length.max() + "\"";
                 }
             } catch (NoSuchFieldException ex) {
-
+                System.out.println(ex);
             }
 
         }
