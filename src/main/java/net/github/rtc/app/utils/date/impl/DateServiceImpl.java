6734ea89e79091118da17c6f6d86cf65ac2dc28c
Karpov Ilya <karpov-illya@bigmir.net>
2015-01-25 16:25:34 +0200

    Wrong "term in month" on News Details page

--- src/main/java/net/github/rtc/app/utils/date/impl/DateServiceImpl.java
+++ src/main/java/net/github/rtc/app/utils/date/impl/DateServiceImpl.java
@@ -5,6 +5,7 @@ import org.joda.time.*;
 import org.springframework.stereotype.Component;
 
 
+import java.util.Calendar;
 import java.util.Date;
 
 @Component("DateService")
@@ -22,7 +23,26 @@ public class DateServiceImpl implements DateService {
 
     @Override
     public int getMothPeriod(final Date startDate, final Date endDate) {
-        final Period period = new Period(new DateTime(startDate), new DateTime(endDate), PeriodType.months());
-        return period.getMonths() + 1;
+        int months = new Period(new DateTime(startDate), new DateTime(endDate), PeriodType.months()).getMonths();
+        if (months == 0) {
+            ++months;
+        } else {
+            int days = new Period(new DateTime(startDate), new DateTime(endDate), PeriodType.days()).getDays();
+            final Calendar calendar = Calendar.getInstance();
+            calendar.setTime(startDate);
+            for (int i = 0; i < months; i++) {
+                final int currentMonthDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+                if (days > currentMonthDays) {
+                    days = days - currentMonthDays;
+                    calendar.add(Calendar.MONTH, 1);
+                }
+            }
+
+            final int term = 14;
+            if (days > term) {
+                ++months;
+            }
+        }
+        return months;
     }
 }
