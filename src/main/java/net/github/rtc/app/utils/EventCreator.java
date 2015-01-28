3c7b881a83567704af8b107dbb43d7e84d4bbffd
r0ndom <r0ndom@yahoo.com>
2015-01-29 00:36:14 +0200

    checkstyle

--- src/main/java/net/github/rtc/app/utils/EventCreator.java
+++ src/main/java/net/github/rtc/app/utils/EventCreator.java
@@ -1,20 +1,12 @@
 package net.github.rtc.app.utils;
 
 
-import net.github.rtc.app.model.activity.Activity;
 import net.github.rtc.app.model.activity.ActivityAction;
 import net.github.rtc.app.model.activity.ActivityEntity;
 import net.github.rtc.app.model.activity.IActivity;
 import net.github.rtc.app.utils.events.ActivityEvent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.servlet.ServletContext;
 public class EventCreator implements ApplicationEventPublisherAware {
 
     private ApplicationEventPublisher publisher;
