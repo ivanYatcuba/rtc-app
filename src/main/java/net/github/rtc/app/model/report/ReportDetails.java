c3ac9e37fadf9e8a069f0a849d9dd4b1c3e703d8
Karpov Ilya <karpov-illya@bigmir.net>
2015-01-29 15:12:24 +0200

    Fix fingBugs malicious code vulnerability errors. Part 3

--- src/main/java/net/github/rtc/app/model/report/ReportDetails.java
+++ src/main/java/net/github/rtc/app/model/report/ReportDetails.java
@@ -83,11 +83,13 @@ public class ReportDetails extends AbstractPersistenceObject {
     }
 
     public Date getCreatedDate() {
-        return createdDate;
+        return new Date(createdDate.getTime());
     }
 
     public void setCreatedDate(final Date createdDate) {
-        this.createdDate = createdDate;
+        if (createdDate != null) {
+            this.createdDate = new Date(createdDate.getTime());
+        }
     }
 
     public List<Field> getFieldsFromClass() throws NoSuchFieldException {
