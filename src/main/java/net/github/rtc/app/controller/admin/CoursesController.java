0026fb27815f4ce67f8b03d94688f14e5e74b8bd
ivanYatcuba <ivan.yatsouba@gmail.com>
2015-02-04 14:23:32 +0200

    course controller refactor

--- src/main/java/net/github/rtc/app/controller/admin/CoursesController.java
+++ src/main/java/net/github/rtc/app/controller/admin/CoursesController.java
@@ -7,15 +7,12 @@ import net.github.rtc.app.model.course.CourseType;
 import net.github.rtc.app.model.user.RoleType;
 import net.github.rtc.app.model.user.User;
 import net.github.rtc.app.service.course.CourseService;
-import net.github.rtc.app.service.date.DateService;
 import net.github.rtc.app.service.user.UserService;
-import net.github.rtc.app.utils.datatable.search.CourseSearchFilter;
 import net.github.rtc.app.utils.propertyeditors.CustomExpertsEditor;
 import net.github.rtc.app.utils.propertyeditors.CustomTagsEditor;
 import net.github.rtc.app.utils.propertyeditors.CustomTypeEditor;
 import net.github.rtc.util.converter.ValidationContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -23,31 +20,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
 
-/**
- * Controller for {@link net.github.rtc.app.model.course.Course}
- *
- * @author Vladislav Pikus
- */
-@Controller("coursesController")
+@Controller
 @RequestMapping("admin/course")
 public class CoursesController implements MenuItem {
 
     private static final String COURSE = "course";
     private static final String TYPES = "types";
     private static final String STATUSES = "statuses";
-    private static final String FILTER_COURSE = "filterCourse";
+    private static final String CATEGORIES = "categories";
+    private static final String TAGS = "tags";
+    private static final String EXPERTS = "experts";
+    private static final String VALIDATION_RULES = "validationRules";
+
     private static final String REDIRECT = "redirect:/";
     private static final String ADMIN = "admin";
-    private static final String VALIDATION_RULES = "validationRules";
-    private static final String EXPERTS = "experts";
-    private static final String VIEW = "view/";
-    private static final String REDIRECT1 = "redirect: ";
     private static final String ROOT = "portal/admin";
+    private static final String VIEW = "view/";
     private static final String UPDATE_VIEW = "/course/courseUpdate";
     private static final String CREATE_VIEW = "/course/courseCreate";
     private static final String DETAILS_VIEW = "/course/courseDetails";
+    private static final String SHOW_MSG_NEWS_CREATED = "?newsJustCreated=true";
 
-    private static final String SHOW_MSG_NEWS_CREATED = "?newsJustCreated=true";;
     @Autowired
     private CourseService courseService;
     @Autowired
@@ -58,26 +51,26 @@ public class CoursesController implements MenuItem {
     @RequestMapping(value = "/create", method = RequestMethod.GET)
     public ModelAndView create() {
         final ModelAndView mav = new ModelAndView(ROOT + CREATE_VIEW);
-        mav.addObject(STRING_VALIDATION_RULES, validationContext.get(Course.class));
+        mav.addObject(VALIDATION_RULES, validationContext.get(Course.class));
         return mav;
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(@ModelAttribute(STRING_COURSE) final Course course,
+    public String save(@ModelAttribute(COURSE) final Course course,
                        @RequestParam(required = false) final boolean ifPublish,
                        @RequestParam(required = false) final boolean ifCreateNews) {
         courseService.saveCourse(ifPublish, ifCreateNews, course, false);
         if (ifCreateNews) {
-            return REDIRECT1 + VIEW + course.getCode() + SHOW_MSG_NEWS_CREATED;
+            return REDIRECT + VIEW + course.getCode() + SHOW_MSG_NEWS_CREATED;
         }
-        return REDIRECT1 + VIEW + course.getCode();
+        return REDIRECT + VIEW + course.getCode();
     }
 
     @RequestMapping(value = VIEW + "{courseCode}", method = RequestMethod.GET)
     public ModelAndView single(@PathVariable final String courseCode,
                                @RequestParam(required = false) boolean newsJustCreated) {
         final ModelAndView mav = new ModelAndView(ROOT + DETAILS_VIEW);
-        mav.addObject(STRING_COURSE, courseService.findByCode(courseCode));
+        mav.addObject(COURSE, courseService.findByCode(courseCode));
         mav.addObject("newsInfo", newsJustCreated);
         return mav;
     }
@@ -85,47 +78,47 @@ public class CoursesController implements MenuItem {
     @RequestMapping(value = "/{courseCode}/update", method = RequestMethod.GET)
     public ModelAndView update(@PathVariable final String courseCode) {
         final ModelAndView mav = new ModelAndView(ROOT + UPDATE_VIEW);
-        mav.addObject(STRING_COURSE, courseService.findByCode(courseCode));
-        mav.addObject(STRING_VALIDATION_RULES, validationContext.get(Course.class));
+        mav.addObject(COURSE, courseService.findByCode(courseCode));
+        mav.addObject(VALIDATION_RULES, validationContext.get(Course.class));
         return mav;
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(@ModelAttribute(STRING_COURSE) final Course course,
+    public String update(@ModelAttribute(COURSE) final Course course,
                          @RequestParam(required = false) final boolean ifPublish,
                          @RequestParam(required = false) final boolean ifCreateNews) {
         courseService.saveCourse(ifPublish, ifCreateNews, course, true);
         if (ifCreateNews) {
-            return REDIRECT1 + VIEW + course.getCode() + SHOW_MSG_NEWS_CREATED;
+            return REDIRECT + VIEW + course.getCode() + SHOW_MSG_NEWS_CREATED;
         }
-        return REDIRECT1 + VIEW + course.getCode();
+        return REDIRECT + VIEW + course.getCode();
     }
 
     @RequestMapping(value = "/delete/{courseCode}", method = RequestMethod.GET)
     public String delete(@PathVariable final String courseCode) {
         courseService.deleteByCode(courseCode);
-        return REDIRECT + STRING_ADMIN;
+        return REDIRECT + ADMIN;
     }
 
     @RequestMapping(value = "/publish/{courseCode}", method = RequestMethod.GET)
     public String publish(@PathVariable final String courseCode,
                           @RequestParam(required = false) final boolean ifCreateNews) {
         courseService.publish(ifCreateNews, courseCode);
-        return REDIRECT + STRING_ADMIN;
+        return REDIRECT + ADMIN;
     }
 
     @RequestMapping(value = "/archive/{courseCode}", method = RequestMethod.GET)
     public String archive(@PathVariable final String courseCode) {
         courseService.archive(courseCode);
-        return REDIRECT + STRING_ADMIN;
+        return REDIRECT + ADMIN;
     }
 
-    @ModelAttribute(value = STRING_COURSE)
+    @ModelAttribute(value = COURSE)
     public Course getCommandObject() {
         return new Course();
     }
 
-    @ModelAttribute("categories")
+    @ModelAttribute(CATEGORIES)
     public Map<String, String> getCategories() {
         final Map<String, String> categories = new HashMap<>();
         for (CourseType type : CourseType.findAll()) {
@@ -134,7 +127,7 @@ public class CoursesController implements MenuItem {
         return categories;
     }
 
-    @ModelAttribute(STRING_EXPERTS)
+    @ModelAttribute(EXPERTS)
     public Map<String, String> getExpertUsers() {
         final Map<String, String> expertMap = new HashMap<>();
         for (User u : userService.getUserByRole(RoleType.ROLE_EXPERT)) {
@@ -142,16 +135,16 @@ public class CoursesController implements MenuItem {
         }
         return expertMap;
     }
-    @ModelAttribute(STRING_STATUSES)
+    @ModelAttribute(STATUSES)
     public Collection<String> getStatuses() {
         return CourseStatus.findAll();
     }
 
-    @InitBinder(STRING_COURSE)
+    @InitBinder(COURSE)
     public void initBinder(final WebDataBinder binder) {
-        binder.registerCustomEditor(List.class, "tags", new CustomTagsEditor());
-        binder.registerCustomEditor(List.class, STRING_TYPES, new CustomTypeEditor());
-        binder.registerCustomEditor(Set.class, STRING_EXPERTS, new CustomExpertsEditor(userService));
+        binder.registerCustomEditor(List.class, TAGS, new CustomTagsEditor());
+        binder.registerCustomEditor(List.class, TYPES, new CustomTypeEditor());
+        binder.registerCustomEditor(Set.class, EXPERTS, new CustomExpertsEditor(userService));
     }
 
     @Override
