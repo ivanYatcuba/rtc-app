2f211d651f300ed83a78dedf6a56c65336fffc77
ivanYatcuba <ivan.yatsouba@gmail.com>
2015-02-24 16:38:53 +0200

    mail service javadoc

--- src/main/java/net/github/rtc/app/service/mail/MailService.java
+++ src/main/java/net/github/rtc/app/service/mail/MailService.java
@@ -3,17 +3,23 @@ package net.github.rtc.app.service.mail;
 
 import org.springframework.mail.SimpleMailMessage;
 
+/**
+ * The service class that is responsible for sending messages on users' email
+ */
 public interface MailService {
+
     /**
-     *
-     * @param from -
-     * @param to
-     * @param subject
-     * @param msg -
+     * Send mail from one user to another
+     * @param from sender address
+     * @param to receiver address
+     * @param subject topic of the mail
+     * @param msg text of the message
      */
     void sendMail(String from, String to, String subject, String msg);
 
+    /**
+     * Send mail message
+     * @param msg mail message
+     */
     void sendMail(SimpleMailMessage msg);
-
-
 }
