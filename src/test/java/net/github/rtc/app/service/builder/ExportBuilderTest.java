28dbb611a7ca30d6f15b2dfef7921128d1a02974
r0ndom <LeNeMu260>
2015-03-05 14:22:34 +0200

    added test to SearchResultsBuilder class

---
+++ src/test/java/net/github/rtc/app/service/builder/ExportBuilderTest.java
@@ -0,0 +1,40 @@
+package net.github.rtc.app.service.builder;
+
+import net.github.rtc.app.model.entity.export.ExportFormat;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class ExportBuilderTest {
+
+    @InjectMocks
+    private ExportBuilder exportBuilder;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testBuild() {
+        List<Field> exportField = new ArrayList<>();
+        List objectsList = new ArrayList();
+        String sheetName = null;
+        String filePath = null;
+        ExportFormat exportFormat = null;
+        exportBuilder.build(exportField, objectsList, sheetName, filePath, exportFormat);
+        assertTrue(true);
+    }
+}
