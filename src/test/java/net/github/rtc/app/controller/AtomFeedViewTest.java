70cb4b6f3687a683ff2c759eace72113cd2fa40a
ivanYatcuba <ivan.yatsouba@gmail.com>
2015-03-10 20:14:32 +0200

    fork test

--- src/test/java/net/github/rtc/app/controller/AtomFeedViewTest.java
+++ src/test/java/net/github/rtc/app/controller/AtomFeedViewTest.java
@@ -20,7 +20,7 @@ import java.util.*;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
-
+//fork test
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:feed-test.xml")
 public class AtomFeedViewTest {
