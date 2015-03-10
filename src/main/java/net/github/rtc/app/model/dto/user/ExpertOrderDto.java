21395282f9f9ae1998a514f824110fd7503f65a9
AlexSamoylov <oosamoylov@gmail.com>
2015-03-10 16:46:36 +0200

    checkstyle

--- src/main/java/net/github/rtc/app/model/dto/user/ExpertOrderDto.java
+++ src/main/java/net/github/rtc/app/model/dto/user/ExpertOrderDto.java
@@ -125,43 +125,72 @@ public class ExpertOrderDto {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ExpertOrderDto)) return false;
-
-        ExpertOrderDto that = (ExpertOrderDto) o;
-
-        if (courseAcceptedOrders != that.courseAcceptedOrders) return false;
-        if (courseCapacity != that.courseCapacity) return false;
-        if (courseCode != null ? !courseCode.equals(that.courseCode) : that.courseCode != null) return false;
-        if (courseEndDate != null ? !courseEndDate.equals(that.courseEndDate) : that.courseEndDate != null)
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ExpertOrderDto)) {
             return false;
-        if (courseName != null ? !courseName.equals(that.courseName) : that.courseName != null) return false;
-        if (courseStartDate != null ? !courseStartDate.equals(that.courseStartDate) : that.courseStartDate != null)
+        }
+
+        final ExpertOrderDto that = (ExpertOrderDto) o;
+
+        if (courseAcceptedOrders != that.courseAcceptedOrders) {
             return false;
-        if (orderCode != null ? !orderCode.equals(that.orderCode) : that.orderCode != null) return false;
-        if (orderDate != null ? !orderDate.equals(that.orderDate) : that.orderDate != null) return false;
-        if (status != that.status) return false;
-        if (userCode != null ? !userCode.equals(that.userCode) : that.userCode != null) return false;
-        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
-        if (userPhoto != null ? !userPhoto.equals(that.userPhoto) : that.userPhoto != null) return false;
+        }
+        if (courseCapacity != that.courseCapacity) {
+            return false;
+        }
+        if (courseCode != null ? !courseCode.equals(that.courseCode) : that.courseCode != null) {
+            return false;
+        }
+        if (courseEndDate != null ? !courseEndDate.equals(that.courseEndDate) : that.courseEndDate != null) {
+            return false;
+        }
+
+        if (courseName != null ? !courseName.equals(that.courseName) : that.courseName != null) {
+            return false;
+        }
+        if (courseStartDate != null ? !courseStartDate.equals(that.courseStartDate) : that.courseStartDate != null) {
+            return false;
+        }
+        if (orderCode != null ? !orderCode.equals(that.orderCode) : that.orderCode != null) {
+            return false;
+        }
+        if (orderDate != null ? !orderDate.equals(that.orderDate) : that.orderDate != null) {
+            return false;
+        }
+        if (status != that.status) {
+            return false;
+        }
+        if (userCode != null ? !userCode.equals(that.userCode) : that.userCode != null) {
+            return false;
+        }
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) {
+            return false;
+        }
+        if (userPhoto != null ? !userPhoto.equals(that.userPhoto) : that.userPhoto != null) {
+            return false;
+        }
 
         return true;
     }
 
     @Override
     public int hashCode() {
+        final int hashCodeConst = 31;
+
         int result = userCode != null ? userCode.hashCode() : 0;
-        result = 31 * result + (userName != null ? userName.hashCode() : 0);
-        result = 31 * result + (userPhoto != null ? userPhoto.hashCode() : 0);
-        result = 31 * result + (courseCode != null ? courseCode.hashCode() : 0);
-        result = 31 * result + (courseName != null ? courseName.hashCode() : 0);
-        result = 31 * result + (courseStartDate != null ? courseStartDate.hashCode() : 0);
-        result = 31 * result + (courseEndDate != null ? courseEndDate.hashCode() : 0);
-        result = 31 * result + courseCapacity;
-        result = 31 * result + courseAcceptedOrders;
-        result = 31 * result + (orderCode != null ? orderCode.hashCode() : 0);
-        result = 31 * result + (orderDate != null ? orderDate.hashCode() : 0);
-        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = hashCodeConst * result + (userName != null ? userName.hashCode() : 0);
+        result = hashCodeConst * result + (userPhoto != null ? userPhoto.hashCode() : 0);
+        result = hashCodeConst * result + (courseCode != null ? courseCode.hashCode() : 0);
+        result = hashCodeConst * result + (courseName != null ? courseName.hashCode() : 0);
+        result = hashCodeConst * result + (courseStartDate != null ? courseStartDate.hashCode() : 0);
+        result = hashCodeConst * result + (courseEndDate != null ? courseEndDate.hashCode() : 0);
+        result = hashCodeConst * result + courseCapacity;
+        result = hashCodeConst * result + courseAcceptedOrders;
+        result = hashCodeConst * result + (orderCode != null ? orderCode.hashCode() : 0);
+        result = hashCodeConst * result + (orderDate != null ? orderDate.hashCode() : 0);
+        result = hashCodeConst * result + (status != null ? status.hashCode() : 0);
         return result;
     }
 }
